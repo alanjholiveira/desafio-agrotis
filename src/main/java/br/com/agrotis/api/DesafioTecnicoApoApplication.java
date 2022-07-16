0cf99690c76551864e66390f63d0d0e9fc405b46
Alan Oliveira <alanjhone@gmail.com>
2022-07-16 20:28:53 -0300

    setup inicial

---
+++ src/main/java/br/com/agrotis/api/DesafioTecnicoApoApplication.java
@@ -0,0 +1,13 @@
+package br.com.agrotis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoApoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTecnicoApoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/agrotis/api/DesafioTecnicoApoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agrotis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
